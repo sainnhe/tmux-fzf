bb5c93d9dc5f0d86ec7c9ed9341def4190df5bed
Sainnhepark <sainnhe@gmail.com>
2019-04-20 00:50:01 +0800

    rewrite main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmux run-shell "$CURRENT_DIR/scripts/$(ls $CURRENT_DIR/scripts | sed -r 's/(.*).{3}$/\1/' | $CURRENT_DIR/scripts/.fzf-tmux $TMUX_FZF_OPTIONS | sed 's/$/.sh/')"
+ITEMS_ORIGIN=$(ls "$CURRENT_DIR/scripts" | sed -r 's/(.*).{3}$/\1/')
+ITEM=$(printf "%s\n[cancel]" "$ITEMS_ORIGIN" | "$CURRENT_DIR/scripts/.fzf-tmux" "$TMUX_FZF_OPTIONS")
+if [[ "$ITEM" == "[cancel]" ]]; then
+    exit
+else
+    ITEM=$(echo "$CURRENT_DIR/scripts/$ITEM" | sed 's/$/.sh/')
+    tmux run-shell "$ITEM"
+fi
