ecd00f9998becd80f96a7d9266d402ae0867c8dd
Sainnhepark <sainnhe@gmail.com>
2019-04-20 01:05:29 +0800

    cancel action in key

--- scripts/key.sh
+++ scripts/key.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmux list-keys | grep -v 'copy-mode' | "$CURRENT_DIR/.fzf-tmux" "$TMUX_FZF_OPTIONS" | sed -r 's/^.{46}//g' | xargs tmux
+TARGET_ORIGIN=$(tmux list-keys)
+TARGET=$(printf "[cancel]\n%s" "$TARGET_ORIGIN" | grep -v 'copy-mode' | "$CURRENT_DIR/.fzf-tmux" "$TMUX_FZF_OPTIONS" | sed -r 's/^.{46}//g')
+if [[ "$TARGET" == "[cancel]" ]]; then
+    exit
+else
+    tmux "$TARGET"
+fi
