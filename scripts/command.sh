20c75e36379a17691e9de9592be29a9de9c1e9c4
Sainnhepark <sainnhe@gmail.com>
2019-04-20 01:01:16 +0800

    cancel action in command

--- scripts/command.sh
+++ scripts/command.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TARGET=$(tmux list-commands | "$CURRENT_DIR/.fzf-tmux" "$TMUX_FZF_OPTIONS" | grep -o '^[^[:blank:]]*')
-tmux command-prompt -I "$TARGET"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TARGET_ORIGIN=$(tmux list-commands)
+TARGET=$(printf "[cancel]\n%s" "$TARGET_ORIGIN" | "$CURRENT_DIR/.fzf-tmux" "$TMUX_FZF_OPTIONS" | grep -o '^[^[:blank:]]*')
+if [[ "$TARGET" == "[cancel]" ]]; then
+    exit
+else
+    tmux command-prompt -I "$TARGET"
+fi
