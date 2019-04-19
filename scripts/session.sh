b9401f9967e6d30c843a9f483b29b4f474964ef4
Sainnhepark <sainnhe@gmail.com>
2019-04-19 23:55:36 +0800

    adjust session

--- scripts/session.sh
+++ scripts/session.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TARGET_ORIGIN=$(printf "$(tmux list-sessions)\ncancel" | "$CURRENT_DIR/.fzf-tmux")
-
-if [[ "$TARGET_ORIGIN" == "cancel" ]]; then
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ACTION=$(printf "attach\ndetach\nkill\nrename\ncancel" | "$CURRENT_DIR/.fzf-tmux")
+if [[ "$ACTION" == "cancel" ]]; then
     exit
 else
-    TARGET=$(printf "$TARGET_ORIGIN" | sed -E 's/:.*$//' | grep -v \"^$(tmux display-message -p '#S')\$\")
-    ACTION=$(printf "attach\ndetach\nkill\nrename\ncancel" | "$CURRENT_DIR/.fzf-tmux")
-    if [[ "$ACTION" == "cancel" ]]; then
+    TARGET_ORIGIN=$(printf "$(tmux list-sessions)\ncancel" | "$CURRENT_DIR/.fzf-tmux")
+    if [[ "$TARGET_ORIGIN" == "cancel" ]]; then
         exit
-    elif [[ "$ACTION" == "attach" ]]; then
-        echo "$TARGET" | xargs tmux switch-client -t
-    elif [[ "$ACTION" == "detach" ]]; then
-        echo "$TARGET" | xargs tmux detach -s
-    elif [[ "$ACTION" == "kill" ]]; then
-        echo "$TARGET" | xargs tmux kill-session -t
-    elif [[ "$ACTION" == "rename" ]]; then
-        tmux command-prompt -I "rename-session -t $TARGET "
+    else
+        TARGET=$(echo "$TARGET_ORIGIN" | sed -E 's/:.*$//' | grep -v \"^$(tmux display-message -p '#S')\$\")
+        if [[ "$ACTION" == "attach" ]]; then
+            echo "$TARGET" | xargs tmux switch-client -t
+        elif [[ "$ACTION" == "detach" ]]; then
+            echo "$TARGET" | xargs tmux detach -s
+        elif [[ "$ACTION" == "kill" ]]; then
+            echo "$TARGET" | xargs tmux kill-session -t
+        elif [[ "$ACTION" == "rename" ]]; then
+            tmux command-prompt -I "rename-session -t $TARGET "
+        fi
     fi
 fi
