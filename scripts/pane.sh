9f6320b627fb82d89753f2c22fa49211677af92a
Sainnhepark <sainnhe@gmail.com>
2019-04-21 21:35:54 +0800

    new: support break pane

--- scripts/pane.sh
+++ scripts/pane.sh
@@ -8,7 +8,7 @@ else
     PANES=$(tmux list-panes -a -F "#S:#{window_index}.#{pane_index}: $TMUX_FZF_PANE_FORMAT")
 fi
 
-ACTION=$(printf "switch\nlayout\nkill\n[cancel]" | "$CURRENT_DIR/.fzf-tmux" "$TMUX_FZF_OPTIONS")
+ACTION=$(printf "switch\nlayout\nkill\nbreak\n[cancel]" | "$CURRENT_DIR/.fzf-tmux" "$TMUX_FZF_OPTIONS")
 if [[ "$ACTION" == "[cancel]" ]]; then
     exit
 elif [[ "$ACTION" == "layout" ]]; then
@@ -30,6 +30,29 @@ else
             echo "$TARGET" | xargs tmux select-pane -t
         elif [[ "$ACTION" == "kill" ]]; then
             echo "$TARGET" | sort -r | xargs -i tmux kill-pane -t {}
+        elif [[ "$ACTION" == "break" ]]; then
+            DST_WIN=$(printf "after\nend\nbegin\n[cancel]" | "$CURRENT_DIR/.fzf-tmux" "$TMUX_FZF_OPTIONS")
+            CUR_WIN_NUM=$(tmux display-message -p | grep -o '[[[:alpha:]|[:digit:]]*] [[:digit:]]*:' | sed -e 's/\[.*\] //' -e 's/.$//')
+            CUR_SES=$(tmux display-message -p | sed -e 's/^.//' -e 's/].*//')
+            LAST_WIN_NUM=$(tmux list-windows | sort -r | sed '2,$d' | sed 's/:.*//')
+            ((LAST_WIN_NUM_AFTER=LAST_WIN_NUM+1))
+            ((CUR_WIN_NUM_AFTER=CUR_WIN_NUM+1))
+            if [[ "$DST_WIN" == "after" ]]; then
+                tmux break-pane -s "$TARGET" -t "$CUR_SES":"$LAST_WIN_NUM_AFTER"
+                tmux new-window -a -t "$CUR_SES":"$CUR_WIN_NUM"
+                LAST_WIN_NUM=$(tmux list-windows | sort -r | sed '2,$d' | sed 's/:.*//')
+                tmux swap-window -s "$CUR_SES":"$LAST_WIN_NUM" -t "$CUR_SES":"$CUR_WIN_NUM_AFTER"
+                tmux kill-window -t "$CUR_SES":"$LAST_WIN_NUM"
+            elif [[ "$DST_WIN" == "end" ]]; then
+                tmux break-pane -s "$TARGET" -t "$CUR_SES":"$LAST_WIN_NUM_AFTER"
+            elif [[ "$DST_WIN" == "begin" ]]; then
+                tmux break-pane -s "$TARGET" -t "$CUR_SES":"$LAST_WIN_NUM_AFTER"
+                tmux new-window -a -t "$CUR_SES":0
+                LAST_WIN_NUM=$(tmux list-windows | sort -r | sed '2,$d' | sed 's/:.*//')
+                tmux swap-window -s "$CUR_SES":"$LAST_WIN_NUM" -t "$CUR_SES":1
+                tmux swap-window -s "$CUR_SES":1 -t "$CUR_SES":0
+                tmux kill-window -t "$CUR_SES":"$LAST_WIN_NUM"
+            fi
         fi
     fi
 fi
