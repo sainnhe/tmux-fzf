06036d504118284c9950481bde275b9e54bbffba
Sainnhepark <sainnhe@gmail.com>
2020-12-05 15:25:26 +0800

    fix all_history

--- scripts/clipboard.sh
+++ scripts/clipboard.sh
@@ -5,7 +5,7 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if ! [ -f "$CURRENT_DIR/../.temp" ]; then
     action="buffer"
-elif [[ -z "$1" ]]; then
+elif [ -z "$1" ]; then
     action=$(printf "all\nbuffer\nsystem\n[cancel]" | eval "$CURRENT_DIR/.fzf-tmux $TMUX_FZF_OPTIONS")
 else
     action="$1"
@@ -15,8 +15,11 @@ fi
 if [[ "$action" == "all" ]]; then
     buffer_clipboard_history=$(tmux list-buffers | sed -E -e 's/^buffer[^/]*bytes: "//' -e 's/"$//')
     system_clipboard_history=$("${CURRENT_DIR}/clipboard/clipster" -on 0)
-    [ -z "$buffer_clipboard_history" ] && buffer_clipboard_history="" || buffer_clipboard_history="${buffer_clipboard_history}\n"
-    all_history=$(printf "%s%s" "${buffer_clipboard_history}" "${system_clipboard_history}" | eval "$CURRENT_DIR/.fzf-tmux $TMUX_FZF_OPTIONS")
+    if [ -z "$buffer_clipboard_history" ]; then
+        all_history=$(printf "%s" "${system_clipboard_history}" | eval "$CURRENT_DIR/.fzf-tmux $TMUX_FZF_OPTIONS")
+    else
+        all_history=$(printf "%s\n%s" "${buffer_clipboard_history}" "${system_clipboard_history}" | eval "$CURRENT_DIR/.fzf-tmux $TMUX_FZF_OPTIONS")
+    fi
     [[ -z "${all_history}" ]] && exit
     tmux send-keys -l "${all_history}"
 elif [[ "$action" == "buffer" ]]; then
