3e261309ad367c3fe56c0ef14af00078684b1035
Sainnhe Park <sainnhe@gmail.com>
2022-08-02 17:53:19 +0800

    Fix keybinding.sh on macOS

--- scripts/keybinding.sh
+++ scripts/keybinding.sh
@@ -4,7 +4,7 @@ FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --header='Select a key binding.'"
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/.envs"
 
-target=$(tmux list-keys | sed '1i [cancel]' | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS")
+target=$(tmux list-keys | sed '1s/^/[cancel]\n/' | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS")
 
 [[ "$target" == "[cancel]" || -z "$target" ]] && exit
 if [[ -n $(echo "$target" | grep -o "copy-mode") && -z $(echo "$target" | grep -o "prefix") ]]; then
