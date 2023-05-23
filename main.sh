253495fb360c4028a2d1eefa251d7dc2169227d8
Michael Bianco <iloveitaly@gmail.com>
2023-05-23 16:58:57 -0600

    feat: add copy-mode menu
    Two problems I ran into:
    
    1. copy-mode commands have very little documentation
    2. when you are setting up keybindings it's hard to know which commands are available and what they do
    
    having this menu would help in those situations, and be useful when you don't have a command bound but want to run it.

--- main.sh
+++ main.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-[[ -z "$TMUX_FZF_ORDER" ]] && TMUX_FZF_ORDER="session|window|pane|command|keybinding|clipboard|process"
+[[ -z "$TMUX_FZF_ORDER" ]] && TMUX_FZF_ORDER="copy-mode|session|window|pane|command|keybinding|clipboard|process"
 source "$CURRENT_DIR/scripts/.envs"
+
+# remove copy-mode from options if we aren't in copy-mode
+if [[ "$TMUX_FZF_ORDER" == *"copy-mode"* ]] && [ "$(tmux display-message -p '#{pane_in_mode}')" -eq 0 ]; then
+    TMUX_FZF_ORDER="$(echo $TMUX_FZF_ORDER | sed -E 's/\|?copy-mode\|?//')"
+fi
 
 items_origin="$(echo $TMUX_FZF_ORDER | tr '|' '\n')"
 if [[ -z "$TMUX_FZF_MENU" ]]; then
