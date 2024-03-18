231c835681ff47a17a99fb4f73307e67a25f52c0
Isidro Arias <isidroariass@hotmail.es>
2024-03-18 20:31:16 +0100

    Use simple commands (cut, head and tail)
    Instead of the powerful sed

--- scripts/keybinding.sh
+++ scripts/keybinding.sh
@@ -9,7 +9,5 @@ target=$(tmux list-keys | sed '1s/^/[cancel]\n/' | eval "$TMUX_FZF_BIN $TMUX_FZF
 [[ "$target" == "[cancel]" || -z "$target" ]] && exit
 if [[ -n $(echo "$target" | grep -o "copy-mode") && -z $(echo "$target" | grep -o "prefix") ]]; then
     tmux copy-mode
-    echo "$target" | sed -E 's/^.{46}//g' | xargs tmux
-else
-    echo "$target" | sed -E 's/^.{46}//g' | xargs tmux
 fi
+echo "$target" | awk '{ $1=$2=$3=$4=""; print $0 }' | sed 's/^ *//' | xargs tmux
