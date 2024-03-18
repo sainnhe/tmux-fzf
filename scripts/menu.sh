84a0396ee9d3e7237b4e583ddaddc11b10d8cebf
Isidro Arias <isidroariass@hotmail.es>
2024-03-18 20:31:16 +0100

    Use simple commands (cut, head and tail)
    Instead of the powerful sed

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -5,15 +5,14 @@ source "$CURRENT_DIR/.envs"
 
 # get front end list
 tmux_fzf_menu_origin=$TMUX_FZF_MENU
-front_end_list=$(echo -e "$tmux_fzf_menu_origin" | sed -n '1p')
-tmux_fzf_menu_origin=$(echo -e "$tmux_fzf_menu_origin" | sed '1,2d')
-while [[ $(echo -e "$tmux_fzf_menu_origin" | wc -l | xargs) != "0" && $(echo -e "$tmux_fzf_menu_origin" | wc -l | xargs) != "1" ]]; do
-    front_end_list="$front_end_list\n"$(echo -e "$tmux_fzf_menu_origin" | sed -n '1p')
-    tmux_fzf_menu_origin=$(echo -e "$tmux_fzf_menu_origin" | sed '1,2d')
+front_end_list=$(echo -e "$tmux_fzf_menu_origin" | head -1 )$'\n'
+tmux_fzf_menu_origin=$(echo -e "$tmux_fzf_menu_origin" | tail -n +3 )
+while [ $(echo -ne "$tmux_fzf_menu_origin" | wc -l ) -ge 2 ]; do
+    front_end_list+=$(echo  "$tmux_fzf_menu_origin" | head -1)$'\n'
+    tmux_fzf_menu_origin=$(echo  "$tmux_fzf_menu_origin" | tail -n +3)$'\n'
 done
-front_end_list=$(echo -e "$front_end_list" | sed '/^[[:space:]]*$/d')
 
-target=$(printf "%s\n[cancel]" "$front_end_list" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS")
+target=$(printf "%s[cancel]" "$front_end_list" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS")
 
 [[ "$target" == "[cancel]" || -z "$target" ]] && exit
 # get the next line in $TMUX_FZF_MENU and execute
