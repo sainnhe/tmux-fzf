0e02006a9015a3b73a1d940cb0737c91af3006d1
Isidro Arias <isidroariass@hotmail.es>
2024-03-18 20:22:59 +0100

    refactor main.sh

--- main.sh
+++ main.sh
@@ -4,17 +4,17 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 [[ -z "$TMUX_FZF_ORDER" ]] && TMUX_FZF_ORDER="copy-mode|session|window|pane|command|keybinding|clipboard|process"
 source "$CURRENT_DIR/scripts/.envs"
 
+items="$(echo $TMUX_FZF_ORDER | tr '|' '\n')"
+
 # remove copy-mode from options if we aren't in copy-mode
-if [[ "$TMUX_FZF_ORDER" == *"copy-mode"* ]] && [ "$(tmux display-message -p '#{pane_in_mode}')" -eq 0 ]; then
-    TMUX_FZF_ORDER="$(echo $TMUX_FZF_ORDER | sed -E 's/\|?copy-mode\|?//')"
+if [ "$(tmux display-message -p '#{pane_in_mode}')" -eq 0 ]; then
+    items_origin="$(echo "${items}" | sed '/copy-mode/d')"
 fi
 
-items_origin="$(echo $TMUX_FZF_ORDER | tr '|' '\n')"
-if [[ -z "$TMUX_FZF_MENU" ]]; then
-    item=$(printf "%s\n[cancel]" "$items_origin" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS")
-else
-    item=$(printf "menu\n%s\n[cancel]" "$items_origin" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS")
+if [[ ! -z "$TMUX_FZF_MENU" ]]; then
+    items_origin+=$'\nmenu'
 fi
+items_origin+=$'\n[cancel]'
+item=$(echo "${items_origin}" | $TMUX_FZF_BIN $TMUX_FZF_OPTIONS )
 [[ "$item" == "[cancel]" || -z "$item" ]] && exit
-item=$(echo "$CURRENT_DIR/scripts/$item" | sed -E 's/$/.sh/')
-tmux run-shell -b "$item"
+tmux run-shell -b "$CURRENT_DIR/scripts/${item}.sh"
