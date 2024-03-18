da6d213d922f9a8cfde8bbd2145dd9046eccbc75
Isidro Arias <isidroariass@hotmail.es>
2024-03-18 20:31:16 +0100

    Use simple commands (cut, head and tail)
    Instead of the powerful sed

--- scripts/command.sh
+++ scripts/command.sh
@@ -5,7 +5,7 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/.envs"
 
 target_origin=$(tmux list-commands)
-target=$(printf "[cancel]\n%s" "$target_origin" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS" | grep -o '^[^[:blank:]]*')
+target=$(printf "[cancel]\n%s" "$target_origin" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS" | cut -d ' ' -f 1)
 
 [[ "$target" == "[cancel]" || -z "$target" ]] && exit
 tmux command-prompt -I "$target"
