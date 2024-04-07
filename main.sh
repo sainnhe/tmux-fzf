608356a7d3787a107fa0ac44c0cc9477b002b460
Sainnhe Park <i@sainnhe.dev>
2024-04-07 19:59:09 +0800

    fix(main.sh): items didn't show in copy mode

--- main.sh
+++ main.sh
@@ -4,11 +4,11 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 [[ -z "$TMUX_FZF_ORDER" ]] && TMUX_FZF_ORDER="copy-mode|session|window|pane|command|keybinding|clipboard|process"
 source "$CURRENT_DIR/scripts/.envs"
 
-items="$(echo $TMUX_FZF_ORDER | tr '|' '\n')"
+items_origin="$(echo $TMUX_FZF_ORDER | tr '|' '\n')"
 
 # remove copy-mode from options if we aren't in copy-mode
 if [ "$(tmux display-message -p '#{pane_in_mode}')" -eq 0 ]; then
-    items_origin="$(echo "${items}" | sed '/copy-mode/d')"
+    items_origin="$(echo "${items_origin}" | sed '/copy-mode/d')"
 fi
 
 if [[ ! -z "$TMUX_FZF_MENU" ]]; then
